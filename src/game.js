6f3a2a42e85031c2d841774841b4cb6a1b83c678
Vincent <dakinnie@gmail.com>
2021-06-23 10:37:42 +0300

    JS test

--- src/game.js
+++ src/game.js
@@ -1,1 +1,22 @@
-alert("hello")
+//game initialization
+
+// toggle values
+const colors = [
+  "brown",
+  "chartreuse",
+  "cadetblue",
+  "chocolate",
+  "cornflowerblue",
+  "darkgreen",
+  "darkmagenta",
+  "goldenrod",
+];
+
+//get nodes
+let nodes = document.getElementById("board").childNodes;
+console.log(nodes);
+for (let i = 1; i < 32; i += 2) {
+  nodes[i].addEventListener("click", function () {
+    console.log(`You've clicked the ${(i+1)/2}th div`);
+  });
+}
