9b62fff92902350a360d78227f2c5e0c2a75f360
Vincent <dakinnie@gmail.com>
2021-07-21 13:45:21 +0300

    Reset game logic

--- src/game.js
+++ src/game.js
@@ -33,7 +33,7 @@ function flippedCardsIndex(array) {
 
 //get nodes
 let nodes = document.getElementById("board").childNodes;
-console.log(nodes);
+// console.log(nodes);
 
 //shuffle array
 function shuffleArray(array) {
@@ -48,36 +48,8 @@ shuffleArray(colors);
 //game logic
 for (let i = 1; i < 32; i += 2) {
   nodes[i].addEventListener("click", function () {
-    if (
-      !flipped[(i + 1) / 2 - 1] &&
-      flipped.reduce((acc, val) => acc + val) < 2
-    ) {
-      nodes[i].style.background = colors[(i + 1) / 2 - 1];
-      flipped[(i + 1) / 2 - 1] = 1;
-      console.log(flipped);
-
-      if (flipped.reduce((acc, val) => acc + val) == 2) {
-        //reset styles
-        console.log(colors);
-        if (
-          colors[flippedCardsIndex(flipped)[0]] ==
-          colors[flippedCardsIndex(flipped)[1]]
-        ) {
-          for (let item of flippedCardsIndex(flipped)) {
-            setTimeout(function () {
-              nodes[(item - 1) * 2 + 3].style.background = "white";
-              flipped = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-            }, 1000);
-          }
-        } else {
-          setTimeout(function () {
-            for (let i = 1; i < 32; i += 2) {
-              nodes[i].style.background = "#bdadad";
-            }
-            flipped = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-          }, 1000);
-        }
-      }
+    if (true) {
+      
     }
   });
 }
